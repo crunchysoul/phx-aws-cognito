89552f0b7727f5a266189d7e9336e2820b453f84
crunchysoul <zzzcca12@gmail.com>
2018-05-07 23:25:42 +1000

    initialize and finish

---
+++ lib/phx_aws_cognito_web/controllers/fallback_controller.ex
@@ -0,0 +1,20 @@
+defmodule PhxAwsCognitoWeb.FallbackController do
+  @moduledoc """
+  Translates controller action results into valid `Plug.Conn` responses.
+
+  See `Phoenix.Controller.action_fallback/1` for more details.
+  """
+  use PhxAwsCognitoWeb, :controller
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> render(PhxAwsCognitoWeb.ChangesetView, "error.json", changeset: changeset)
+  end
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> render(PhxAwsCognitoWeb.ErrorView, :"404")
+  end
+end
--- lib/phx_aws_cognito_web/endpoint.ex
+++ lib/phx_aws_cognito_web/endpoint.ex
@@ -1,41 +1,57 @@
 defmodule PhxAwsCognitoWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :phx_aws_cognito
 
-  socket "/socket", PhxAwsCognitoWeb.UserSocket
+  socket("/socket", PhxAwsCognitoWeb.UserSocket)
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/", from: :phx_aws_cognito, gzip: false,
+  plug(
+    Plug.Static,
+    at: "/",
+    from: :phx_aws_cognito,
+    gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt)
+  )
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    plug Phoenix.CodeReloader
+    plug(Phoenix.CodeReloader)
   end
 
-  plug Plug.Logger
+  plug(Plug.Logger)
 
-  plug Plug.Parsers,
+  plug(
+    Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Poison
+  )
 
-  plug Plug.MethodOverride
-  plug Plug.Head
+  plug(Plug.MethodOverride)
+  plug(Plug.Head)
 
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Session,
+  plug(
+    Plug.Session,
     store: :cookie,
     key: "_phx_aws_cognito_key",
     signing_salt: "Ayz94dWY"
+  )
 
-  plug PhxAwsCognitoWeb.Router
+  plug(
+    Corsica,
+    origin: "http://localhost:8080",
+    log: [rejected: :error, invalid: :warn, accepted: :debug],
+    allow_headers: ["content-type"],
+    allow_credentials: true
+  )
+
+  plug(PhxAwsCognitoWeb.Router)
 
   @doc """
   Callback invoked for dynamically configuring the endpoint.
